51c8cf13bc2e1f011a9cba1f4b8b804b0a9fbf29
[varunsaini417] <[varunsaini417@gmail.com]>
2022-03-21 21:46:04 +0530

    added map fumction in card to reduce the mltpl code

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -3,37 +3,23 @@ import data from "./cardsData";
 const Cards = () => {
   return (
     <>
-      
-        <div className="row  d-flex justify-content-around p-sm-3 mb-5">
-          <div className="col-lg-4">
-            <div className="card-deck">
-            <img className="img-fluid" src="/Images/3.jpg" alt="" />
-              <div className="card shadow p-3 mb-5 bg-white rounded">
-                <h2>{data[0].tittle}</h2>
-                <p className="mt-5">{data[0].parah}</p>
+      <div className="row  d-flex justify-content-around p-sm-3 mb-5">
+        {data.map((detail) => {
+          return (
+            <div className="col-lg-4">
+              <div className="card-deck">
+                <img className="img-fluid" src={detail.src} alt="" />
+                <div className="card shadow p-3 mb-5 bg-white rounded">
+                  <h2>{detail.tittle}</h2>
+                  <p className="mt-5">{detail.parah}</p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-lg-4">
-            <div className="card-deck">
-            <img className="img-fluid" src="/Images/3.jpg" alt="" />
-              <div className="card shadow p-3 mb-5 bg-white rounded">
-                <h2>{data[1].tittle}</h2>
-                <p className="mt-5">{data[1].parah}</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-4">
-            <div className="card-deck">
-            <img className="img-fluid" src="/Images/3.jpg" alt="" />
-              <div className="card p-3 shadow p-3 mb-5 bg-white rounded">
-                <h2>{data[2].tittle}</h2>
-                <p className="mt-5">{data[2].parah}</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      
+          );
+        })}
+
+       
+      </div>
     </>
   );
 };
