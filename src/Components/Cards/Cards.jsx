438fc17d19281f343387dab6546b98b58b589201
[varunsaini417] <[varunsaini417@gmail.com]>
2022-03-21 21:50:28 +0530

    added heading above the cards

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -4,9 +4,10 @@ const Cards = () => {
   return (
     <>
       <div className="row  d-flex justify-content-around p-sm-3 mb-5">
+      <h1 className="text-center">Learn what we can do for you?</h1>
         {data.map((detail) => {
           return (
-            <div className="col-lg-4">
+            <div className="col-lg-4 mt-5">
               <div className="card-deck">
                 <img className="img-fluid" src={detail.src} alt="" />
                 <div className="card shadow p-3 mb-5 bg-white rounded">
