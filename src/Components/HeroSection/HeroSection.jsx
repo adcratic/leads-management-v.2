86f5dd5614ea2f860f23206db3aa40a797f316fe
[varunsaini417] <[varunsaini417@gmail.com]>
2022-03-21 23:38:24 +0530

    added effect on heroSecImage

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import "../HeroSection/hero-section.css";
 import Cards from "../Cards/Cards";
+import parallex from "../../Effects/Parallex/parallex";
+
 const HeroSection = (props) => {
   return (
     <>
       <div style={{ backgroundColor: "#FFFFFF" }}>
-        <div
-          className="container mw-50  pt-5 "
-          
-        >
+        <div className="container mw-50  pt-5 ">
           <div className="row t-5">
             <div className="col-lg-5 ">
               <div className="h1">Your business deservers this.</div>
               <div className="h4 text-muted pt-5">
-                See and check leads in one place.  LMS as your work manager,
+                See and check leads in one place. LMS as your work manager,
                 you’ll stay on top of everything, your competitors are doing.
               </div>
 
@@ -37,16 +36,30 @@ const HeroSection = (props) => {
                 </div>
               )}
             </div>
-            <div className="col-lg">
-              <img className="img-fluid" style={{height: "80%"}} src="/Images/2.jpg" alt="" />
+            <div className="col-lg effectImage" onMouseMove={parallex}>
+              <img
+                className="img-fluid"
+                style={{ height: "80%" }}
+                src="/Images/2.jpg"
+                alt=""
+              />
             </div>
           </div>
-
-          <Cards/>         
+          <section id="home-card-section">
+            <Cards />
+          </section>
+          <section id="home-check-section">
+            <div className="h1 text-center mb-5">
+              How we can help you to increase your business?
+            </div>
+            <div className="row">
+              <div className="col">
+              <i class="bi bi-check"></i>
+              </div>
+            </div>
+          </section>
         </div>
       </div>
-
-
     </>
   );
 };
