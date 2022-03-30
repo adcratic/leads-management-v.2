b0fddc328afffbd51874d6f0d56ef37faba4701c
[varunsaini417] <[varunsaini417@gmail.com]>
2022-03-30 17:28:25 +0530

    sign up completed

--- src/pages/SignUp/Signup.jsx
+++ src/pages/SignUp/Signup.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-// import { Link } from "react-router-dom";
-import "../SignUp/SignUp.css"
+import { Link } from "react-router-dom";
+import "../SignUp/SignUp.css";
 const Signup = () => {
   return (
     <>
-      <section class="vh-100 ">
-        <div class="container py-5 h-100">
-        <div className="logo h2"> Logo</div>
+      <section class=" ">
+        <div class="container py-5 ">
+          <div className="logo h2"> Logo</div>
           <div class="row justify-content-center align-items-center h-100 mt-5">
-            <div class="col-12 col-lg-9 col-xl-7">
+            <div class="col-12 col-lg-9 col-xl-7 mb-5">
               <div
                 class="card shadow-2-strong card-registration"
-                style={{borderRadius : "15px"}}
+                style={{ borderRadius: "15px" }}
               >
                 <div class="card-body p-4 p-md-5">
                   <h3 class="mb-4 pb-2 pb-md-0 mb-md-5">Registration Form</h3>
@@ -95,7 +95,6 @@ const Signup = () => {
                           </label>
                         </div>
                       </div>
-                      
                     </div>
 
                     <div class="row">
@@ -107,12 +106,10 @@ const Signup = () => {
                             id="birthdayDate"
                           />
                           <label for="birthdayDate" class="form-label">
-                            Business Category* 
+                            Business Category*
                           </label>
                         </div>
                       </div>
-                      
-                      
                     </div>
                     <div class="mt-4 pt-2">
                       <input
@@ -128,6 +125,30 @@ const Signup = () => {
           </div>
         </div>
       </section>
+      <footer
+        style={{ backgroundColor: "#080B0F" }}
+        className="text-center text-lg-start  text-muted"
+      >
+        <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom container"></section>
+
+        <section className="footer">
+          <div className="container text-center text-md-start mt-5">
+            <div className="row mt-3">
+              <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4 ">
+                Adcratic Studios
+              </div>
+                
+              <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                @2022
+              </div>
+
+              <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                Features
+              </div>
+            </div>
+          </div>
+        </section>
+      </footer>
     </>
   );
 };
